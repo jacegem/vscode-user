dc4c28ccfb2f9d4fa1f2c383051c75718a777714
JACE <jacegem@gmail.com>
2023-12-26 09:17:41 +0900

    add vim normal

--- src/settings/vim/main.clj
+++ src/settings/vim/main.clj
@@ -1,10 +1,21 @@
-(ns settings.vim.main)
+(ns settings.vim.main
+  (:require [clojure.data.json :as json]
+            [settings.vim.mode.insert :refer [vim-insert]]
+            [settings.vim.mode.normal :refer [vim-normal]]))
 
 
 
+(defn write-settings []
+  (let [file-name "settings.json"
+        settings (json/read-str (slurp file-name))
+        config (merge settings
+                      (vim-insert)
+                      (vim-normal))
+        json-str (json/write-str config {:escape-slash false
+                                         :escape-unicode true})]
+    (spit file-name json-str)))
 
 
-
-(defn read-settings [])
-
-(defn write-settings [])
+(comment
+  (write-settings)
+  :rcf)
--- src/settings/vim/mode/insert.clj
+++ src/settings/vim/mode/insert.clj
@@ -1,1 +1,12 @@
-(ns settings.vim.mode.insert)
+(ns settings.vim.mode.insert
+  (:require [settings.vim.mode.util :refer [convert-after]]))
+
+
+(def before-after
+  {[:j :j] :esc
+   ["ㅓ" "ㅓ"] :esc})
+
+
+(defn vim-insert []
+  {"vim.insertModeKeyBindingsNonRecursive"
+   (convert-after before-after)})
